eeae156c8c5a83ad4fb3493ba1c23c91a7983048
Jay Kapadiya <jayk.mobio@gmail.com>
2022-06-21 16:13:24 +0530

    Created program on sets and added methods to it

---
+++ sets.py
@@ -0,0 +1,9 @@
+set1 = {1,2,4,8,10}
+set2 = {2,10,12,14,16}
+set3 = set1.union(set2)
+print(set3)
+set3 = set1.intersection(set2)
+print(set3)
+set3 = set1.difference(set2)
+set3.clear()
+print(set3)
