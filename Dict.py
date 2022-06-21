fee2d53b7fc3ce0481a15eb8bd33c59343782f0e
Jay Kapadiya <jayk.mobio@gmail.com>
2022-06-21 15:31:06 +0530

    Dictionary methods

---
+++ Dict.py
@@ -0,0 +1,15 @@
+dict1 = {1: 'msft', 2: 'Software', 3: 'Microsoft Technologies'}
+dict1[4] = 'Operating System'
+print(dict1)
+dict1 = {1: 'msft', 2: 'Software', 3: 'Microsoft Technologies', 4: 'Operating System'}
+print(dict1)
+dict1['Bill Gates'] = ['Owner']
+print(dict1)
+dict1 = {1: 'msft', 2: 'Software', 3: 'Microsoft Technologies', 4: 'Operating System',}
+print(dict1)
+dict1['Bill Gates'] = ['Owner']
+print(dict1)
+del dict1['Bill Gates']
+print(dict1)
+dict1.clear()
+print(dict1)
