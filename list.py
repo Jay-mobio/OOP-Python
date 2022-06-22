f1e8ea10c4839d0fdde6aff570e1b82fa0d89fa9
Jay Kapadiya <jayk.mobio@gmail.com>
2022-06-22 17:25:12 +0530

    list

---
+++ list.py
@@ -0,0 +1,13 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+print(fruits)
+print(fruits.count('apple'))
+print(fruits.count('grapes'))
+print(fruits.index('banana'))
+print(fruits.reverse())
+fruits.append('grape')
+print(fruits)
+fruits.sort()
+print(fruits)
+fruits.pop()
+print(fruits)
+
